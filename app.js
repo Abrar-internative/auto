f3e53729d086aeb25fda1eb3a37052086bc8b36e
Abrar-internative <abrar.internativelabs@gmail.com>
2023-07-31 15:11:25 +0500

    update

--- app.js
+++ app.js
@@ -1,42 +1,59 @@
 const fs = require("fs");
+const path = require("path");
 const ejs = require("ejs");
-const pluralize = require('pluralize');
+const pluralize = require("pluralize");
 
 const capitalize = require("./utility/capitalize");
 const createOrUpdateFile = require("./utility/createOrUpdateFile");
 
 const entityName = process.argv[2];
 
-const directories = ['route', 'controller', 'service', 'validation'];
+const directories = ["route", "controller", "service", "validation"];
 
-const validations = ['create', 'update'];
+const validations = ["create", "update"];
 
-directories.forEach(directory => {
+directories.forEach((directory) => {
+  if (directory === "validation") {
+    validations.forEach((validation) => {
+      const starterTemplate = fs.readFileSync(
+        `${__dirname}/template/${validation}${capitalize(
+          directory
+        )}.template.ejs`,
+        "utf8"
+      );
+      const template = ejs.render(starterTemplate, {
+        entityName,
+        capitalize,
+        plural: pluralize.plural,
+      });
 
+      createOrUpdateFile(
+        `${directory}s/${entityName}/${entityName}${capitalize(
+          validation
+        )}.${directory}.js`,
+        template
+      );
+    });
+  } else {
+    const starterTemplate = fs.readFileSync(
+      `${__dirname}/template/${directory}.template.ejs`,
+      "utf8"
+    );
+    const template = ejs.render(starterTemplate, {
+      entityName,
+      capitalize,
+      plural: pluralize.plural,
+    });
 
-    if (directory === 'validation') {
+    createOrUpdateFile(
+      `${directory}s/${entityName}/${entityName}.${
+        directory === "route" ? directory + "s" : directory
+      }.js`,
+      template
+    );
+  }
 
-        validations.forEach(validation =>{
-
-            const starterTemplate = fs.readFileSync(`template/${validation}${capitalize(directory)}.template.ejs`, 'utf8');
-            const template = ejs.render(starterTemplate, { entityName, capitalize, plural: pluralize.plural });
-    
-            createOrUpdateFile(
-                `${directory}s/${entityName}/${entityName}${capitalize(validation)}.${directory}.js`, template
-            );
-        })
-        
-    } else {
-
-        const starterTemplate = fs.readFileSync(`template/${directory}.template.ejs`, 'utf8');
-        const template = ejs.render(starterTemplate, { entityName, capitalize, plural: pluralize.plural });
-        
-        createOrUpdateFile(
-            `${directory}s/${entityName}/${entityName}.${directory === 'route' ? directory + 's' : directory}.js`, template
-        );
-    }
-
-    console.log(`${entityName} ${directory}s created successfully.`);
+  console.log(`${entityName} ${directory}s created successfully.`);
 });
 
 console.log(`All done.`);
