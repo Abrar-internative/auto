e01fcfdc76ac109dcc68c65db4997f52b5aae9a9
Abrar-internative <abrar.internativelabs@gmail.com>
2023-07-31 14:05:35 +0500

    init

---
+++ app.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const ejs = require("ejs");
+const pluralize = require('pluralize');
+
+const capitalize = require("./utility/capitalize");
+const createOrUpdateFile = require("./utility/createOrUpdateFile");
+
+const entityName = process.argv[2];
+
+const directories = ['route', 'controller', 'service', 'validation'];
+
+const validations = ['create', 'update'];
+
+directories.forEach(directory => {
+
+
+    if (directory === 'validation') {
+
+        validations.forEach(validation =>{
+
+            const starterTemplate = fs.readFileSync(`template/${validation}${capitalize(directory)}.template.ejs`, 'utf8');
+            const template = ejs.render(starterTemplate, { entityName, capitalize, plural: pluralize.plural });
+    
+            createOrUpdateFile(
+                `${directory}s/${entityName}/${entityName}${capitalize(validation)}.${directory}.js`, template
+            );
+        })
+        
+    } else {
+
+        const starterTemplate = fs.readFileSync(`template/${directory}.template.ejs`, 'utf8');
+        const template = ejs.render(starterTemplate, { entityName, capitalize, plural: pluralize.plural });
+        
+        createOrUpdateFile(
+            `${directory}s/${entityName}/${entityName}.${directory === 'route' ? directory + 's' : directory}.js`, template
+        );
+    }
+
+    console.log(`${entityName} ${directory}s created successfully.`);
+});
+
+console.log(`All done.`);
